a0987c52fee9fa317c8401139580836d2abcff45
MacBookPro <macbookpro@MacBookPros-MBP.attlocal.net>
2020-09-15 14:45:07 -0500

    Applied a Navbar with a brand, temporary functionless links and basic styling.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import '../styles/navbar.css';
 
 export default class Navbar extends Component {
     render() {
         return (
             <div className="nav-container">
-                
+
+                {/* Brand */}
+                <div className="brand">
+                    <span className="brand-left">MRP</span> 
+                    <span className="brand-split">|</span> 
+                    <span className="brand-right">T</span>
+                </div>
+
+                {/* Navbar Links */}
+                <div className="link-list">
+                    <ul className="links">
+                        <li>
+                            <a className="link" href="#">Projects</a>
+                        </li>
+                        <li>
+                            <a className="link" href="#">Blog</a>
+                        </li>
+                        <li>
+                            <a className="link" href="#">Home</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         )
     }
