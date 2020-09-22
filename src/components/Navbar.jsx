d1268f330b6392343f83729ef0a2801ad12c78f7
MacBookPro <macbookpro@MacBookPros-MBP.attlocal.net>
2020-09-22 15:24:10 -0500

    Updated most links in the navbar to be working.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/navbar.css';
+import resume from '../mattpdev-resume.pdf';
 
 export default class Navbar extends Component {
     render() {
@@ -21,34 +22,41 @@ export default class Navbar extends Component {
 
                     <li className="nav-item">
                         <a href="/" className="nav-link">
+                            <i className="fas fa-home"></i>
+                            <span className="link-text">Home</span>
+                        </a>
+                    </li>
+
+                    <li className="nav-item">
+                        <a href="/" className="nav-link">
                             <i className="fas fa-project-diagram"></i>
                             <span className="link-text">Projects</span>
                         </a>
                     </li>
 
                     <li className="nav-item">
-                        <a href="/" className="nav-link">
+                        <a href="https://www.linkedin.com/in/matthewpalmer9" className="nav-link">
                             <i className="fab fa-linkedin"></i>
                             <span className="link-text">LinkedIn</span>
                         </a>
                     </li>
 
                     <li className="nav-item">
-                        <a href="/" className="nav-link">
+                        <a href="https://www.twitter.com/MattPDev" className="nav-link">
                             <i className="fab fa-twitter"></i>
                             <span className="link-text">Twitter</span>
                         </a>
                     </li>
 
                     <li className="nav-item">
-                        <a href="/" className="nav-link">
+                        <a href="https://www.github.com/matthewpalmer9" className="nav-link">
                             <i className="fab fa-github"></i>
                             <span className="link-text">Github</span>
                         </a>
                     </li>
 
                     <li className="nav-item">
-                        <a href="/" className="nav-link">
+                        <a href={resume} className="nav-link" download>
                             <i className="fas fa-download"></i>
                             <span className="link-text">Resume</span>
                         </a>
